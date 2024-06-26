e9805b8e75b26b4f2d8c24fdea0a6b93b965b9aa
Jannis Leifeld <j.leifeld@shopware.com>
2024-06-26 13:46:27 +0200

    NEXT-36570 - change pinia store to local app instance

--- @tool/setup-env-for-shopware.js
+++ @tool/setup-env-for-shopware.js
@@ -27,12 +27,13 @@ const envBefore = process.env.NODE_ENV;
 process.env.NODE_ENV = 'production';
 const configureCompat = require(resolve(join(srcPath, 'node_modules/@vue/compat/dist/vue.cjs.js'))).configureCompat;
 
-// Enable Pinia support
-const vueUse = require(resolve(join(srcPath, 'node_modules/@vue/compat/dist/vue.cjs.js'))).use;
-vueUse(Shopware.Store._rootState);
-
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 configureCompat(compatConfig);
+
+// Enable Pinia Support
+const { createApp } = require(resolve(join(srcPath, 'node_modules/@vue/compat/dist/vue.cjs.js')));
+const app = createApp();
+app.use(Shopware.Store._rootState)
 
 process.env.NODE_ENV = envBefore;
 
